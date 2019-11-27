575dfe745fb1fd73838d73a5cfc78c43cfc66e7f
Andreas Søvik <andreas@andreasrs.com>
2019-11-27 22:41:02 +0100

    Set sane security-relate http headers

--- src/server/index.js
+++ src/server/index.js
@@ -25,6 +25,15 @@ router.get('/projects', (ctx) => { ctx.body = projects; });
 router.get('/contact', (ctx) => { ctx.body = contact; });
 
 app.use(logger());
+app.use(async (ctx, next) => {
+  ctx.response.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+  ctx.response.set('Content-Security-Policy', 'default-src \'self\'; script-src \'self\' \'unsafe-inline\' https://www.google-analytics.com; font-src \'self\' https://fonts.gstatic.com; style-src \'self\' https://cdnjs.cloudflare.com https://fonts.googleapis.com; img-src \'self\' https://www.google-analytics.com');
+  ctx.response.set('X-Frame-Options', 'SAMEORIGIN');
+  ctx.response.set('X-Content-Type-Options', 'nosniff');
+  ctx.response.set('Referrer-Policy', 'strict-origin');
+  ctx.response.set('Feature-Policy', 'microphone \'none\'; camera \'none\'');
+  await next();
+});
 app.use(async (ctx, next) => { ctx.response.set('max-age', cacheTime); await next(); });
 app.use(serve(path.resolve(__dirname, '../www/assets'), { maxage: cacheTime * 1000 }));
 app.use(serve(path.resolve(__dirname, '../www/gfx'), { maxage: cacheTime * 1000 }));
