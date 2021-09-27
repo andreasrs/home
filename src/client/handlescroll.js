bdf7f582a2304eae864ac84cbe6e89d301856dfc
Andreas Søvik <andreas@andreasrs.com>
2021-09-27 21:21:42 +0200

    Remove references to figure element

--- src/client/handlescroll.js
+++ src/client/handlescroll.js
@@ -2,16 +2,13 @@ import throttle from "./throttle";
 
 let previousScroll = window.scrollY;
 const navElement = document.querySelector("nav");
-const figureElement = document.querySelector("figure");
 
 const addSticky = () => {
   navElement.classList.add("sticky");
-  figureElement.classList.add("sticky-follow");
 };
 
 const removeSticky = () => {
   navElement.classList.remove("sticky");
-  figureElement.classList.remove("sticky-follow");
 };
 
 const scrollingUp = () => window.scrollY > 0 && window.scrollY < previousScroll;
