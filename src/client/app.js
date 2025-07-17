9bc9a96ed95b05417d8e5018b8454bc69fab67d6
Andreas Søvik <andreas@andreasrs.com>
2025-07-17 20:46:31 +0200

    Bump stuff

--- src/client/app.js
+++ src/client/app.js
@@ -3,4 +3,5 @@ import "@fontsource/electrolize/index.css";
 import "@fontsource/lato/index.css";
 import "./app.scss";
 import handleScroll from "./handlescroll";
+
 window.addEventListener("scroll", handleScroll);
